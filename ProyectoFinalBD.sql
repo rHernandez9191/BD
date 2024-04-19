df819f1568cda82df5ee080a3b32d9648b81f2a4
JovanGV <jjbrenesv@ucenfotec.ac.cr>
2024-04-18 18:10:20 -0600

    Update ProyectoFinalBD.sql

--- ProyectoFinalBD.sql
+++ ProyectoFinalBD.sql
@@ -3,12 +3,13 @@ CREATE DATABASE ProyectoGymnasio
 USE ProyectoGymnasio
 
 CREATE TABLE TPersona(
-			idPersona int not null PRIMARY KEY,
+			idPersona VARCHAR(45) not null PRIMARY KEY,
 			nombre varchar(45),
 			cedula int,
 			fechaNacimiento DATE,
 			email varchar(45),
-			telefono int)
+			telefono VARCHAR(45)
+			)
 
 CREATE TABLE TGymnasio (
 			idGymnasio int not null PRIMARY KEY,
@@ -38,7 +39,7 @@ CREATE TABLE TPagos(
 
 CREATE TABLE TInstructor(
 			idInstructor int not null PRIMARY KEY,
-			idPersona int,
+			idPersona VARCHAR(45),
 			idPago int,
 			idSede int
 			
@@ -63,7 +64,7 @@ CREATE TABLE TCertificaciones(
 
 CREATE TABLE TUAdministrativo(
 			idUsuario int not null PRIMARY KEY,
-			idPersona int not null,
+			idPersona VARCHAR(45) not null,
 			idPago int not null
 
 			CONSTRAINT FK_PersonaAdmin FOREIGN  KEY (idPersona)
@@ -85,18 +86,18 @@ CREATE TABLE TMediciones(
 
 CREATE TABLE TCliente(
 			idCliente int not null PRIMARY KEY,
-			idPersona int not null,
+			idPersona VARCHAR(45) not null,
 			estatura float,
 			sexo varchar(1),
 			altoRiesgo varchar(2),
 			idInstructor int not null,
 
-			CONSTRAINT FK_PersonaCliente FOREIGN  KEY (idPersona)
+			CONSTRAINT FK_PersonaCliente FOREIGN KEY (idPersona)
 			REFERENCES TPersona (idPersona),
 
 			CONSTRAINT FK_InstructorCli FOREIGN KEY (idInstructor)
 			REFERENCES TInstructor(idInstructor)
-			)
+			)[dbo].[TCertificaciones]
 
 CREATE TABLE TRutina(
 			idRutina int PRIMARY KEY,
@@ -243,7 +244,7 @@ CREATE TABLE ClienteXInstructor(
 			)
 
 CREATE TABLE PersonaXInstructor(
-			idPersona int not null,
+			idPersona VARCHAR(45) not null,
 			idInstructor int not null
 			CONSTRAINT PersonaXInstructorIP PRIMARY KEY (idPersona, idInstructor),
 			
@@ -255,7 +256,7 @@ CREATE TABLE PersonaXInstructor(
 			)
 
 CREATE TABLE PersonaXUAdmin(
-			idPersona int not null,
+			idPersona VARCHAR(45) not null,
 			idUsuario int not null
 			CONSTRAINT PersonaXAdmin PRIMARY KEY (idPersona, idUsuario),
 			
@@ -267,7 +268,7 @@ CREATE TABLE PersonaXUAdmin(
 			)
 
 CREATE TABLE PersonaXCliente(
-			idPersona int not null,
+			idPersona VARCHAR(45) not null,
 			idCliente int not null
 			CONSTRAINT PersonaXClientePC PRIMARY KEY (idPersona, idCliente),
 			
@@ -278,7 +279,64 @@ CREATE TABLE PersonaXCliente(
 			REFERENCES TCliente(idCliente)
 			)
 
---fn_calcular_imc: Crear una función escalar que recibe el peso y estatura del cliente y retorna el valor decimal del IMC. (2 pts)
+-- Insertar persona ---
+CREATE OR ALTER PROCEDURE insertar_persona (
+	 @nombre varchar(45), @cedula varchar(45), @fechaNacimiento DATE, @email varchar(45), @telefono int
+	)
+AS
+BEGIN
+	DECLARE @idPersona VARCHAR(45)
+	SET @idPersona = LEFT(@nombre, 2) + '-' + LEFT(@cedula,4) + '-' + RIGHT(@fechaNacimiento, 2)
+
+	INSERT INTO TPersona VALUES(@idPersona, @nombre, @cedula, @fechaNacimiento, @email, @telefono)
+END
+
+--Ingresar 10 personas -- 
+
+EXEC insertar_persona 'Juan', '118390739', '09-03-2002', 'juan.segura@gmail.com', 22440987
+EXEC insertar_persona 'Pedro', '236547865', '12-12-2005', 'pedro.lalo@hotmail.com', 87654322
+EXEC insertar_persona 'Alejandro', '22445678', '01-11-1998', 'papasconatun@gmail.com', 98765678
+EXEC insertar_persona 'Giuliana', '125433456', '05-05-1988', 'peinadoazul@yahoo.com', 76678558
+EXEC insertar_persona 'Lucia', '238777654', '30-12-1995', 'luciamesen@gmail.com', 90908776
+EXEC insertar_persona 'Lucia Ramirez', '238777654', '1995-12-30', 'luciaramirez@example.com', 90908776;
+EXEC insertar_persona 'Juan Pérez', '564321987', '1988-06-15', 'juanperez@example.com', 12345678;
+EXEC insertar_persona 'María González', '789654123', '1990-03-05', 'maria.gonzalez@example.com', 98765432;
+EXEC insertar_persona 'Pedro Martínez', '112233445', '1985-09-18', 'pedromartinez@example.com', 55556666;
+EXEC insertar_persona 'Ana López', '777888999', '1977-07-22', 'ana.lopez@example.com', 99998888;
+EXEC insertar_persona 'Carlos Fernandez', '456789012', '2000-04-10', 'carlosfernandez@example.com', 11112222;
+
+
+CREATE OR ALTER PROCEDURE insertar_cliente (
+	@idPersona , @estatura, @sexo, @altoRiesgo, @idInstructor
+)
+AS
+BEGIN
+
+	DECLARE @idCliente INT;
+	DECLARE @idPersona Varchar(45);
+	SET @idPersona = 'An-2244-11'
+	SELECT cedula FROM TPersona WHERE @idPersona = TPersona.idPersona
+
+
+	CONVERT(SELECT cedula FROM TPersona WHERE idPersona = TPersona.idPersona, INT);
+
+			([idCliente]
+           ,[idPersona]
+           ,[estatura]
+           ,[sexo]
+           ,[altoRiesgo]
+           ,[idInstructor])
+
+
+	--DECLARE @idPersona VARCHAR(45)
+	--SET @idPersona = LEFT(@nombre, 2) + '-' + LEFT(@cedula,4) + '-' + RIGHT(@fechaNacimiento, 2)
+
+	--INSERT INTO TPersona VALUES(@idPersona, @nombre, @cedula, @fechaNacimiento, @email, @telefono)--
+
+END
+SELECT * FROM TPersona
+
+
 
 --Calcular IMC de una persona fn_calcular_imc--
 
@@ -377,9 +435,11 @@ BEGIN
 	RETURN @resultado
 END 
 
+-- Ejecucion de fn_edad --
+
 SELECT dbo.fn_edad ('09-03-2002')
 
-
+-- Calcular vasos de agua -- 
 CREATE OR ALTER FUNCTION fn_vasos_agua (
 	@peso INT
 )
@@ -394,3 +454,73 @@ BEGIN
 END
 
 SELECT dbo.fn_vasos_agua(70)
+
+CREATE OR ALTER FUNCTION fn_cant_proteina (
+	
+)
+
+CREATE PROCEDURE dbo.pa_obtener_medidas_ideales(
+	@idMedicion
+)
+AS
+BEGIN
+	
+	SELECT Sexo = 'M' FROM TCliente AS C 
+	INNER JOIN
+	TExpediente AS E 
+	ON C.idCliente = E.idCliente
+
+
+
+TMediciones(
+			idMedicion int not null PRIMARY KEY,
+			fecha date,
+			hora time,
+			peso float,
+			porcentajeGrasa float,
+			porcentajeGViceral float,
+			IMC float
+			)
+
+
+TPersona(
+			idPersona int not null PRIMARY KEY,
+			nombre varchar(45),
+			cedula int,
+			fechaNacimiento DATE,
+			email varchar(45),
+			telefono int)
+
+TCliente(
+			idCliente int not null PRIMARY KEY,
+			idPersona int not null,
+			estatura float,
+			sexo varchar(1),
+			altoRiesgo varchar(2),
+			idInstructor int not null,
+
+			CONSTRAINT FK_PersonaCliente FOREIGN  KEY (idPersona)
+			REFERENCES TPersona (idPersona),
+
+			CONSTRAINT FK_InstructorCli FOREIGN KEY (idInstructor)
+			REFERENCES TInstructor(idInstructor)
+
+			CONSTRAINT FK_ExpedienteCli FOREIGN KEY (idExpediente)
+			REFERENCES TExpediente(idExpediente)
+			)
+
+TExpediente(
+			idExpediente int PRIMARY KEY,
+			idCliente int not null,
+			idMedicion int,
+			idRutina int 
+
+			CONSTRAINT FK_ExpedienteRutina FOREIGN KEY (idRutina)
+			REFERENCES TRutina (idRutina),
+
+			CONSTRAINT FK_ExpedienteCli FOREIGN KEY (idCliente)
+			REFERENCES TCliente(idCliente),
+			
+			CONSTRAINT FK_ExpedienteMedi FOREIGN KEY (idMedicion)
+			REFERENCES TMediciones(idMedicion)
+			)
