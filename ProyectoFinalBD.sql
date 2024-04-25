e343426a886ec44961b5744f69d70a1f013c8fb6
JovanGV <jjbrenesv@ucenfotec.ac.cr>
2024-04-24 18:20:25 -0600

    Update ProyectoFinalBD.sql

--- ProyectoFinalBD.sql
+++ ProyectoFinalBD.sql
@@ -96,14 +96,12 @@ CREATE TABLE TCliente(
 			REFERENCES TPersona (idPersona),
 
 			CONSTRAINT FK_InstructorCli FOREIGN KEY (idInstructor)
-			REFERENCES TInstructor(idInstructor)
-			)
+			REFERENCES TInstructor(idInstructor) )
 
 CREATE TABLE TRutina(
 			idRutina int PRIMARY KEY,
 			ejercicion varchar(45),
-			repeticiones int
-			)
+			repeticiones int )
 
 CREATE TABLE TExpediente(
 			idExpediente int PRIMARY KEY,
@@ -113,181 +111,150 @@ CREATE TABLE TExpediente(
 
 			CONSTRAINT FK_ExpedienteRutina FOREIGN KEY (idRutina)
 			REFERENCES TRutina (idRutina),
-
 			CONSTRAINT FK_ExpedienteCli FOREIGN KEY (idCliente)
 			REFERENCES TCliente(idCliente),
-			
 			CONSTRAINT FK_ExpedienteMedi FOREIGN KEY (idMedicion)
-			REFERENCES TMediciones(idMedicion)
-			)
+			REFERENCES TMediciones(idMedicion) )
 
 ----- INTERMEDIAS
 CREATE TABLE ExpedienteXCliente(
 			idExpediente int,
 			idCliente VARCHAR(45)
-			CONSTRAINT PK_ExpedienteXCliente PRIMARY KEY(idExpediente, idCliente),
 
+			CONSTRAINT PK_ExpedienteXCliente PRIMARY KEY(idExpediente, idCliente),
 			CONSTRAINT ExpeXcliente FOREIGN KEY (idExpediente)
 			REFERENCES TExpediente(idExpediente),
-
 			CONSTRAINT ClienteExpe FOREIGN KEY (idCliente)
-			REFERENCES TCliente (idCliente)
-			)
+			REFERENCES TCliente (idCliente) )
 
 CREATE TABLE ExpedienteXMedicion(
 			idExpediente int,
 			idMedicion int
-			CONSTRAINT PK_ExpedienteXMedicion PRIMARY KEY(idExpediente, idMedicion),
 
+			CONSTRAINT PK_ExpedienteXMedicion PRIMARY KEY(idExpediente, idMedicion),
 			CONSTRAINT ExpeMedicion FOREIGN KEY (idExpediente)
 			REFERENCES TExpediente(idExpediente),
-
 			CONSTRAINT MedicionExpe FOREIGN KEY (idMedicion)
-			REFERENCES TMediciones (idMedicion)
-			)
+			REFERENCES TMediciones (idMedicion) )
 
 CREATE TABLE ExpedienteXRutina(
 			idExpediente int,
 			idRutina int
-			CONSTRAINT PK_ExpedienteXRutina PRIMARY KEY(idExpediente, idRutina),
 
+			CONSTRAINT PK_ExpedienteXRutina PRIMARY KEY(idExpediente, idRutina),
 			CONSTRAINT ExpeRutina FOREIGN KEY (idExpediente)
 			REFERENCES TExpediente(idExpediente),
-
 			CONSTRAINT RutinaExpe FOREIGN KEY (idRutina)
-			REFERENCES TRutina (idRutina)
-			)
-
+			REFERENCES TRutina (idRutina) )
 
 CREATE TABLE SedeXGymnasio(
 			idSede VARCHAR(45) not null,
 			idGymnasio VARCHAR(45) not null
+
 			CONSTRAINT SedeXGym PRIMARY KEY (idSede, idGymnasio),
-			
 			CONSTRAINT FK_SedeXGymS FOREIGN KEY (idSede)
 			REFERENCES TSede (idSede),
-
 			CONSTRAINT FK_SedeXGymG FOREIGN KEY (idGymnasio)
-			REFERENCES TGymnasio (idGymnasio)		
-			)
+			REFERENCES TGymnasio (idGymnasio) )
 
 CREATE TABLE PagoXInstructor(
 			idPago VARCHAR(45) not null,
 			idInstructor VARCHAR(45) not null
+
 			CONSTRAINT PagoXinstruc PRIMARY KEY(idPago, idInstructor),
-			
 			CONSTRAINT FK_PagoXInstrucP FOREIGN KEY (idPago)
 			REFERENCES TPagos (idPago),
-			
 			CONSTRAINT FK_PagoXInstrucI FOREIGN KEY (idInstructor)
-			REFERENCES TInstructor (idInstructor)
-			)
+			REFERENCES TInstructor (idInstructor) )
 
 CREATE TABLE CertificacionXInstructor(
 			idCertificacion int not null,
 			idInstructor VARCHAR(45) not null
+
 			CONSTRAINT PagoXInstructorCI PRIMARY KEY(idCertificacion, idInstructor)
-			
 			CONSTRAINT FK_CertiXInstrucCer FOREIGN KEY (idCertificacion)
 			REFERENCES TCertificaciones (idCertificacion),
-			
 			CONSTRAINT FK_CertiXInstrucI FOREIGN KEY (idInstructor)
-			REFERENCES TInstructor (idInstructor)
-			)
+			REFERENCES TInstructor (idInstructor) )
 
 CREATE TABLE InstructorXSede(
 			idInstructor VARCHAR(45) not null,
 			idSede VARCHAR(45) not null
-			CONSTRAINT InstructXSede PRIMARY KEY(idInstructor, idSede),
 
+			CONSTRAINT InstructXSede PRIMARY KEY(idInstructor, idSede),
 			CONSTRAINT FK_InstructorXSedeIns FOREIGN KEY (idInstructor)
 			REFERENCES TInstructor (idInstructor),
-
 			CONSTRAINT FK_InstructorXSedeSe FOREIGN KEY (idSede)
-			REFERENCES TSede (idSede)
-			)
+			REFERENCES TSede (idSede) )
 
 CREATE TABLE PagoXUAdmin(
 			idPago VARCHAR(45) not null,
 			idUsuario int not null
+
 			CONSTRAINT PagoXAdmin PRIMARY KEY(idPago, idUsuario),
-			
 			CONSTRAINT FK_PagoXUAdminU FOREIGN KEY (idUsuario)
 			REFERENCES TUAdministrativo (idUsuario),
-			
 			CONSTRAINT FK_PagoXUAdminP FOREIGN KEY (idPago)
-			REFERENCES TPagos (idPago)
-			)
+			REFERENCES TPagos (idPago) )
 
 CREATE TABLE ClienteXMedicion(
 			idCliente VARCHAR(45) not null,
 			idMedicion int not null,
-			CONSTRAINT ClienteXMedicionCM PRIMARY KEY(idCliente, idMedicion),
 
+			CONSTRAINT ClienteXMedicionCM PRIMARY KEY(idCliente, idMedicion),
 			CONSTRAINT ClienteXMedicionC FOREIGN KEY (idCliente)
 			REFERENCES TCliente (idCliente),
-
 			CONSTRAINT ClienteXMedicionM FOREIGN KEY (idMedicion)
-			REFERENCES TMediciones (idMedicion)
-			)
+			REFERENCES TMediciones (idMedicion) )
 
 CREATE TABLE ClienteXInstructor(
 			idCliente VARCHAR(45) not null,
 			idInstructor VARCHAR(45) not null
+
 			CONSTRAINT ClienteXInstructorCI PRIMARY KEY (idCliente, idInstructor),
-			
 			CONSTRAINT ClienteXInstructorC FOREIGN KEY(idCliente)
 			REFERENCES TCliente (idCliente),
-
 			CONSTRAINT ClienteXInstructorInsructor FOREIGN KEY (idInstructor)
-			REFERENCES TInstructor(idInstructor)
-			)
+			REFERENCES TInstructor(idInstructor) )
 
 CREATE TABLE PersonaXInstructor(
 			idPersona VARCHAR(45) not null,
 			idInstructor VARCHAR(45) not null
+
 			CONSTRAINT PersonaXInstructorIP PRIMARY KEY (idPersona, idInstructor),
-			
 			CONSTRAINT PersonaXInstructorP FOREIGN KEY(idPersona)
 			REFERENCES TPersona (idPersona),
-
 			CONSTRAINT PersonaXInstructorI FOREIGN KEY (idInstructor)
-			REFERENCES TInstructor(idInstructor)
-			)
+			REFERENCES TInstructor(idInstructor) )
 
 CREATE TABLE PersonaXUAdmin(
 			idPersona VARCHAR(45) not null,
 			idUsuario int not null
+
 			CONSTRAINT PersonaXAdmin PRIMARY KEY (idPersona, idUsuario),
-			
 			CONSTRAINT PersonaXAdminP FOREIGN KEY(idPersona)
 			REFERENCES TPersona (idPersona),
-
 			CONSTRAINT PersonaXAdminUA FOREIGN KEY (idUsuario)
-			REFERENCES TUAdministrativo(idUsuario)
-			)
+			REFERENCES TUAdministrativo(idUsuario) )
 
 CREATE TABLE PersonaXCliente(
 			idPersona VARCHAR(45) not null,
 			idCliente VARCHAR(45) not null
+
 			CONSTRAINT PersonaXClientePC PRIMARY KEY (idPersona, idCliente),
-			
 			CONSTRAINT PersonaXClienteP FOREIGN KEY(idPersona)
 			REFERENCES TPersona (idPersona),
-
 			CONSTRAINT PersonaXClienteC FOREIGN KEY (idCliente)
-			REFERENCES TCliente(idCliente)
-			)
+			REFERENCES TCliente(idCliente) )
 
 -- Insertar persona ---
+
 CREATE OR ALTER PROCEDURE insertar_persona (
-	 @nombre varchar(45), @cedula varchar(45), @fechaNacimiento DATE, @email varchar(45), @telefono int
-	)
+	 @nombre varchar(45), @cedula varchar(45), @fechaNacimiento DATE, @email varchar(45), @telefono int )
 AS
 BEGIN
 	DECLARE @idPersona VARCHAR(45)
 	SET @idPersona = LEFT(@nombre, 2) + '-' + LEFT(@cedula,4) + '-' + RIGHT(@fechaNacimiento, 2)
-
 	INSERT INTO TPersona VALUES(@idPersona, @nombre, @cedula, @fechaNacimiento, @email, @telefono)
 END
 
@@ -305,18 +272,14 @@ EXEC insertar_persona 'Pedro Martínez', '112233445', '1985-09-18', 'pedromartine
 EXEC insertar_persona 'Ana López', '777888999', '1977-07-22', 'ana.lopez@example.com', 99998888;
 EXEC insertar_persona 'Carlos Fernandez', '456789012', '2000-04-10', 'carlosfernandez@example.com', 11112222;
 
-SELECT * FROM TPersona
-
 --Insertar pago--
 
 CREATE OR ALTER PROCEDURE insertar_pago (
-	@rol VARCHAR(45), @monto INT
-)
+	@rol VARCHAR(45), @monto INT )
 AS
 BEGIN
 	DECLARE @idPago VARCHAR(45);
 	SET @idPago = LEFT(@rol,2)
-
 	INSERT INTO TPagos VALUES(@idPago, @rol, @monto)
 END
 
@@ -330,20 +293,15 @@ EXEC insertar_pago 'Administrativo', 500000
 EXEC insertar_pago 'Gerente', 450000
 EXEC insertar_pago 'Cajero', 2500000
 
-SELECT * FROM TPagos
-
 --insertar Gymnasio--
 
 CREATE OR ALTER PROCEDURE insertar_gymnasio (
-	@marca VARCHAR(45), @telefonoGeneral VARCHAR(45), @contacto VARCHAR(45)
-)
+	@marca VARCHAR(45), @telefonoGeneral VARCHAR(45), @contacto VARCHAR(45) )
 AS
 BEGIN 
-
 	DECLARE @idGymnasio VARCHAR(45)
 	SET @idGymnasio = LEFT(@marca, 3) + '-' + LEFT(@telefonoGeneral, 3) + '-' + LEFT(@contacto, 3)
 	INSERT INTO TGymnasio VALUES (@idGymnasio, @marca, @telefonoGeneral, @contacto)
-
 END
 
 --Insertar Gymnasio--
@@ -357,8 +315,7 @@ EXEC insertar_gymnasio 'Gimnasio activo', '21213434', 'Pablo Madrid'
 --Insertar sede--
 
 CREATE OR ALTER PROCEDURE insertar_sede (
-	@nomSede VARCHAR(45), @provincia VARCHAR(45), @canton VARCHAR(45), @email VARCHAR(45), @telefono1 VARCHAR(45), @telefono2 VARCHAR(45), @idGymnasio VARCHAR(45)
-)
+	@nomSede VARCHAR(45), @provincia VARCHAR(45), @canton VARCHAR(45), @email VARCHAR(45), @telefono1 VARCHAR(45), @telefono2 VARCHAR(45), @idGymnasio VARCHAR(45) )
 AS
 BEGIN
 	DECLARE @idSede VARCHAR(45)
@@ -367,63 +324,38 @@ BEGIN
 END
 
 --Insertar Sede--
-SELECT * FROM TGymnasio
-
 EXEC insertar_sede 'Sede central', 'San jose', 'San Jose', 'arrozconmango@gmail.com', '89676545', '89348765', 'Al -224-Gen'
-
 
 SELECT * FROM TSede 
 	
-
 --Insertar cliente--
-
 CREATE OR ALTER PROCEDURE insertar_cliente (
-	@idPersona VARCHAR(45), @estatura FLOAT, @sexo VARCHAR(1), @altoRiesgo VARCHAR(2), @idInstructor VARCHAR(45)
-)
+	@idPersona VARCHAR(45), @estatura FLOAT, @sexo VARCHAR(1), @altoRiesgo VARCHAR(2), @idInstructor VARCHAR(45))
 AS
 BEGIN
-
 	DECLARE @idCliente VARCHAR(45);
 	SET @idCliente = @idPersona + '-' + @sexo + '-' + @altoRiesgo + '-' + LEFT(@idInstructor,2)
-
 	INSERT INTO TCliente VALUES(@idCliente, @idPersona, @estatura, @sexo, @altoRiesgo, @idInstructor)
-
 END
 
-
 --Insertar cliente--
---idInstructor, idPErsona, idPago, idSede--
-
-
-
-
 EXEC insertar_cliente 'Al-2244-11', 1.8, 'M', 'Si', 
 
 --Calcular IMC de una persona fn_calcular_imc--
-
-CREATE FUNCTION fn_calcular_imc (
-    @estatura float,
-	@peso float
-	)
+CREATE FUNCTION fn_calcular_imc ( @estatura float, @peso float )
 RETURNS int
 AS
 BEGIN
-   
     DECLARE @IMC float
-
     SET @IMC = @peso / @estatura
-
     RETURN @IMC
 END
 
 --Ejecucion fn_calcular_imc--
-
 SELECT dbo.fn_calcular_imc(5.2,10) AS Resultado
 
 --Clasificar IMC--
-CREATE or ALTER FUNCTION fn_clasificar_imc ( 
-
-	@IMC FLOAT
+CREATE or ALTER FUNCTION fn_clasificar_imc ( @IMC FLOAT
 
 )
 RETURNS VARCHAR(45)
@@ -520,12 +452,9 @@ CREATE OR ALTER FUNCTION fn_cant_proteina (
 	
 )
 
-CREATE PROCEDURE dbo.pa_obtener_medidas_ideales(
-	@idMedicion
-)
+CREATE PROCEDURE dbo.pa_obtener_medidas_ideales( @idMedicion )
 AS
 BEGIN
-	
 	SELECT Sexo = 'M' FROM TCliente AS C 
 	INNER JOIN
 	TExpediente AS E 
