92b6461d9d4240bea9008d25f8d6f7c1383a1ddc
JovanGV <jjbrenesv@ucenfotec.ac.cr>
2024-04-26 18:36:33 -0600

    Update ProyectoFinalBDFuncionesProcesos.sql

--- ProyectoFinalBDFuncionesProcesos.sql
+++ ProyectoFinalBDFuncionesProcesos.sql
@@ -59,9 +59,7 @@ END
 SELECT dbo.fn_clasificar_imc(35) AS Resultado
 
 -- Calcular la edad --
-CREATE OR ALTER FUNCTION fn_edad (
-	@fechaNacimiento DATE
-)
+CREATE OR ALTER FUNCTION fn_edad ( @fechaNacimiento DATE )
 RETURNS INT
 AS
 BEGIN
@@ -83,9 +81,7 @@ END
 SELECT dbo.fn_edad ('09-03-2002')
 
 -- Calcular vasos de agua -- 
-CREATE OR ALTER FUNCTION fn_vasos_agua (
-	@peso INT
-)
+CREATE OR ALTER FUNCTION fn_vasos_agua ( @peso INT )
 RETURNS INT
 AS
 BEGIN
@@ -98,6 +94,82 @@ END
 
 SELECT dbo.fn_vasos_agua(70)
 
+----Cantidad de proteina----
+CREATE OR ALTER FUNCTION fn_cant_proteina(@sexo VARCHAR(1), @peso INT)
+RETURNS INT 
+AS
+BEGIN
+	DECLARE @resultado INT
+	IF (@sexo = 'H')
+		SET @resultado = @peso * 2.5
+	ELSE IF (@sexo = 'M')
+		SET @resultado = @peso * 1.8
+	RETURN @resultado
+END
+
+----Cantidad de proteina----
+SELECT dbo.fn_cant_proteina('M', 70)
+
+----Medidas ideales----
+CREATE OR ALTER PROCEDURE obtener_medidas_ideales(@estatura FLOAT, @sexo VARCHAR(1), @imc FLOAT, @grasaCorporal FLOAT, @grasaViseral FLOAT)
+AS
+BEGIN
+	---select de la pasada medicion para saber si mejoro---
+	DECLARE @resultadoIMC VARCHAR(15)
+	DECLARE @resultadoGrasaCorporal VARCHAR(45)
+	DECLARE @resultadoGrasaViseral VARCHAR(45)
+
+	IF (@sexo = 'H')
+		IF (@estatura  >= 160 AND @estatura < 190)
+			IF (@imc < 18.5)
+				SET @resultadoIMC = 'Bajo'
+			ELSE IF (@imc > 18.5 AND @imc <24.9)
+				SET @resultadoIMC = 'Dentro del rango'
+			ELSE IF (@imc > 24.9)
+				SET @resultadoIMC = 'Alto'
+	ELSE IF (@sexo = 'M')
+		IF (@estatura >= 150 AND @estatura < 180)
+			IF (@imc < 18.5)
+				SET @resultadoIMC = 'Bajo'
+			ELSE IF (@imc > 18.5 AND @imc <24.9)
+				SET @resultadoIMC = 'Dentro del rango'
+			ELSE IF (@imc > 24.9)
+				SET @resultadoIMC = 'Alto'
+
+	IF (@sexo = 'H')
+		IF (@estatura >= 160 AND @estatura < 190)
+			IF (@grasaCorporal < 10)
+				SET @resultadoGrasaCorporal = 'Bajo'
+			ELSE IF (@grasaCorporal >= 10 AND @grasaCorporal < 20)
+				SET @resultadoGrasaCorporal = 'Dentro del rango'
+			ELSE IF (@grasaCorporal > 20)
+				SET @resultadoGrasaCorporal = 'Alto'
+	ELSE IF (@sexo = 'M')
+		IF (@estatura >= 150 AND @estatura < 180)
+			IF (@grasaCorporal < 18)
+				SET @resultadoGrasaCorporal = 'Bajo '
+			ELSE IF (@grasaCorporal >18 AND @grasaCorporal < 28)
+				SET @resultadoGrasaCorporal = 'Dentro del rango'
+			ELSE IF (@grasaCorporal > 28)
+				SET @resultadoGrasaCorporal = 'Alto'
+
+	IF (@sexo = 'H')
+		IF(@estatura > 160 AND @estatura < 190)
+			IF (@grasaViseral < 10)
+				SET @resultadoGrasaViseral = 'Bajo'
+			IF (@grasaViseral = 10)
+				SET @resultadoGrasaViseral = 'Dentro del rango'
+			IF (@grasaViseral > 10)
+				SET @resultadoGrasaViseral = 'Alto'
+	IF (@sexo = 'M')
+		IF (@estatura > 150 AND @estatura < 180)
+			IF (@grasaViseral < 15)
+				SET @resultadoGrasaViseral = 'Bajo'
+			IF (@grasaViseral = 15)
+				SET @resultadoGrasaViseral = 'Dentro del rango'
+			IF (@grasaViseral > 15)
+				SET @resultadoGrasaViseral = 'Alto'
+END
 
 
 ------Procedimiento Creacion de Rutina-----
